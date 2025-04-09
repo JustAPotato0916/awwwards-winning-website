144c9b088b6c12b96bf44b5cb2f820d78c7733d2
JustAPotato0916 <justapotato0916@gmail.com>
2025-04-09 15:35:26 +0800

    feat: about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from './components/About.jsx';
 import Hero from './components/Hero.jsx';
 import React from 'react';
 
@@ -5,8 +6,7 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="z-0 min-h-screen bg-blue-500"></section>
+      <About />
     </main>
   );
 };
