8481d00a3b5511e00f30c4bb69165c6e920d8fdb
JustAPotato0916 <justapotato0916@gmail.com>
2025-04-09 20:58:18 +0800

    feat: features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from './components/About.jsx';
+import Features from './components/Features.jsx';
 import Hero from './components/Hero.jsx';
 import Navbar from './components/Navbar.jsx';
 import React from 'react';
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
